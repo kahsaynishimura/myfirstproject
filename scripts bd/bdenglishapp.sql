3b735da6212b4b16f18524242e25e83806031024
Karina Cristina Sayuri Nishimura <kahsaynishimura@gmail.com>
2015-12-23 14:34:43 -0200

    scripts

--- scripts bd/bdenglishapp.sql
+++ scripts bd/bdenglishapp.sql
@@ -1,8 +1,6 @@
 
-INSERT INTO `script_entry` VALUES (1,'verbs','','',1,1,1);
-INSERT INTO `script_entry` VALUES (2,'to drink','to drink','to drink = beber',2,1,2);
-INSERT INTO `script_entry` VALUES (3,'to eat','to eat','to eat= comer',2,1,3);
-INSERT INTO `lesson` VALUES (1,'Lesson 1',1);
+INSERT INTO `book` VALUES (1,'Teens 2');
+INSERT INTO `lesson` VALUES (10,'Lesson 1',1);
 INSERT INTO `function` VALUES (1,'Only Read text - tts');
 INSERT INTO `function` VALUES (2,'Read text
 Show sentence- tts
@@ -13,7 +11,7 @@ INSERT INTO `function` VALUES (4,'shows video and asks for audio input then chec
 INSERT INTO `function` VALUES (5,'only shows video');
 
 INSERT INTO `exercise` VALUES (1,'Verbs','verbs',1);
-INSERT INTO `book` VALUES (1,'Teens 2');
+
 
 INSERT INTO `user`(name) VALUES ('Karina');
 INSERT INTO `user`(name) VALUES ('Thiago');
